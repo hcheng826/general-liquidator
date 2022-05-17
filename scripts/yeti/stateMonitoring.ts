f97b3fb384428f4c29d07bd56b0f726bfb39f569
hcheng826 <lilioo826@gmail.com>
2022-05-17 09:44:05 +0800

    cold cache complete

--- scripts/yeti/stateMonitoring.ts
+++ scripts/yeti/stateMonitoring.ts
@@ -1,5 +1,7 @@
 import { Position } from "./types";
 import { troveManagerContract } from "./init";
+import { ethers } from "ethers";
+import fs from 'fs';
 
 // hot cache is the positions that we want to closely look at (at the edge of being liquidated)
 export function readFromHotCache(): Array<Position> {
@@ -12,28 +14,41 @@ export function estimateProfitBeforeGas(position: Position): number {
     return 0;
 }
 
-export function updateHotCache() {}
+export async function updateHotCache() {}
 
 // cold cache includes more positions. it's refreshed more infrequently
 export async function updateColdCache() {
-    // console.log('troveManagerContract:', troveManagerContract);
     const trovesCount = (await troveManagerContract.getTroveOwnersCount()).toNumber();
-    // console.log('trovesCount', trovesCount);
-    let troves: Array<Position> = new Array<Position>();
-    // for (let i = 0; i < trovesCount; i++) {
-    for (let i = 0; i < 10; i++) {
-        const troveOwnerAddress = await troveManagerContract.TroveOwners(i);
-        console.log('trove', i, troveOwnerAddress);
-        const ICR = await troveManagerContract.getCurrentICR(troveOwnerAddress);
-        const AICR = await troveManagerContract.getCurrentAICR(troveOwnerAddress);
-        troves.push({
-            borrowerAddress: troveOwnerAddress,
-            ICR,
-            AICR
+    let trovesPromises: Array<Promise<Position>> = new Array<Promise<Position>>();
+
+    for (let i = 0; i < trovesCount; i++) {
+        const trovePromise = troveManagerContract.TroveOwners(i).then((troveOwnerAddress: string) => {
+            return troveManagerContract.getCurrentICR(troveOwnerAddress).then((ICR: ethers.BigNumber) => {
+                return troveManagerContract.getCurrentAICR(troveOwnerAddress).then((AICR: ethers.BigNumber) => {
+                    return {
+                        borrowerAddress: troveOwnerAddress,
+                        ICR,
+                        AICR
+                    };
+                });
+            });
         });
+        trovesPromises.push(trovePromise);
     }
+
+    const troves = await Promise.all(trovesPromises);
     troves.sort((a, b) => {
-        return a.ICR - b.ICR;
-    })
-    console.log(troves);
+        return a.ICR.gt(b.ICR) ? 1 : -1;
+    });
+
+    fs.writeFileSync('./scripts/yeti/cache/allTroves.json', JSON.stringify(troves.map(trove => {
+        return {
+            borrowerAddress: trove.borrowerAddress,
+            // @ts-ignore
+            ICR: trove.ICR.toString()/1e18,
+            // @ts-ignore
+            AICR: trove.AICR.toString()/1e18,
+        }
+    })));
+    return troves
 }
--- scripts/yeti/types.ts
+++ scripts/yeti/types.ts
@@ -1,8 +1,10 @@
+import { ethers } from "ethers";
+
 export type Position = {
     // will definitely add more info such as borrower address, token address, etc.
     borrowerAddress: string,
-    ICR: number,
-    AICR: number,
+    ICR: ethers.BigNumber,
+    AICR: ethers.BigNumber,
     profitBeforeGasFromPosition?: number,
     netProfit?: number,
 }
